b7259e63925cd6ec7cf05faeb254cfca59014cc8
YONGKyy <yongkeo9@gmail.com>
2025-07-16 17:35:41 +0700

    feat: update header layout for improved responsiveness and styling

--- app/layouts/header.vue
+++ app/layouts/header.vue
@@ -13,8 +13,8 @@
 </script>
 
 <template>
-  <header class="min-h-full  bg-darkPurpleHeader">
-    <div class="max-w- mx-auto flex items-center justify-between px-8">
+  <header class="min-h-full  bg-darkPurpleHeader sticky top-0 z-50">
+    <div class=" flex items-center justify-between px-16">
       <!-- Logo -->
       <div class="text-white text-2xl font-bold">
         <span
